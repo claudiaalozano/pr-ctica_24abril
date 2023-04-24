d2dfdc1da797f48fed6cae21006bd8a7b11bb1c5
claudiaalozano <clau.lozano.perez@gmail.com>
2023-04-24 17:03:15 +0200

    ccc

--- banco.py
+++ banco.py
@@ -2,7 +2,7 @@ import asyncio
 
 class Banco:
     def __init__(self, saldo):
-        self.saldo = saldo
+        self.saldo = 100
     
     async def sacar(self, valor1):
         await asyncio.sleep(1)
@@ -17,10 +17,19 @@ class Banco:
 
 
 async def main():
-    banco = Banco(100)
-    await asyncio.gather(40 * banco.depositar(100))
-    await asyncio.gather(20 * banco.depositar(50))
-    await asyncio.gather(60 * banco.depositar(20))
-    await asyncio.gather(20 * banco.sacar(50))
-    await asyncio.gather(60 * banco.sacar(20))
-    await asyncio.gather(40 * banco.sacar(100))
+    banco = Banco()
+    depositar100 = await asyncio.gather(banco.depositar(100))
+    depositar50 = await asyncio.gather(20 * banco.depositar(50))
+    depositar20 = await asyncio.gather(60 * banco.depositar(20))
+    sacar50 = await asyncio.gather(20 * banco.sacar(50))
+    sacar20 = await asyncio.gather(60 * banco.sacar(20))
+    sacar100 = await asyncio.gather(40 * banco.sacar(100))
+
+    tareas = []
+    for saldo in depositar100 + depositar20 + depositar50:
+        tareas.append(asyncio.create_task(banco.depositar(saldo)))
+    await asyncio.gather(*tareas)
+    for saldo in sacar50 + sacar20 + sacar100:
+        tareas.append(asyncio.create_task(banco.sacar(saldo)))
+    await asyncio.gather(*tareas)
+    print(f'Saldo final: {banco.getSaldo()}')
